6ec1863d5573ecd99f43d0382d1688366c345f70
Danilo Metzker <danilometzker@gmail.com>
2025-08-01 04:09:57 -0400

    feat: checkin report

---
+++ src/app/(app)/checkin-details/index.tsx
@@ -0,0 +1,321 @@
+import AppHeader from "@components/AppHeader";
+import BoardingHeader from "@components/BoardingHeader";
+import {
+    router,
+    useFocusEffect,
+    useLocalSearchParams,
+    useNavigation,
+} from "expo-router";
+import { useEffect, useRef, useState } from "react";
+import {
+    StyleSheet,
+    View,
+    Alert,
+    Text,
+    ScrollView,
+    BackHandler,
+    Platform,
+    TouchableOpacity,
+} from "react-native";
+import LoadingScreen from "@components/LoadingScreen";
+import { CheckinInfos } from "@type/checkins";
+import { finishBoarding, getBoarding } from "@services/boarding";
+import { getVehiclesList } from "@services/vehicles";
+import { VehiclesList } from "@type/vehicles";
+import { getBoardingCheckins, getCheckin } from "@services/checkin";
+import CheckinDetailsRow from "@components/CheckinDetailsRow";
+import React from "react";
+import SubmitButton from "@components/SubmitButton";
+import BottomSheet, {
+    BottomSheetBackdrop,
+    BottomSheetBackdropProps,
+    BottomSheetView,
+} from "@gorhom/bottom-sheet";
+import WarningIcon from "@assets/images/warning-checkin.svg";
+import Toast from "react-native-toast-message";
+import { captureRef } from "react-native-view-shot";
+import * as FileSystem from "expo-file-system";
+import { BluetoothEscposPrinter } from "react-native-bluetooth-escpos-printer";
+import ReportPrintView, { PrinterRefProps } from "@components/ReportPrintView";
+import { Boarding } from "@type/boardings";
+
+import PrintIcon from "@assets/icons/action-print.svg";
+import SendIcon from "@assets/icons/action-send.svg";
+import RefundIcon from "@assets/icons/action-refund.svg";
+import CloseIcon from "@assets/icons/close-modal.svg";
+
+const Page = () => {
+    const params = useLocalSearchParams();
+    const [boardingData, setBoardingData] = useState<Boarding>();
+    const [checkinData, setCheckinData] = useState<CheckinInfos>();
+    const [loading, setLoading] = useState(true);
+    const [vehicles, setVehiclesList] = useState<VehiclesList>([]);
+    const navigation = useNavigation();
+
+    // const compRef = useRef<PrinterRefProps>(null);
+
+    useEffect(() => {
+        const loadCheckin = async () => {
+            try {
+                const [data, vehicles] = await Promise.all([
+                    getCheckin(params.checkin_id as unknown as number),
+                    getVehiclesList(),
+                ]);
+
+                if (data) {
+                    setCheckinData(data);
+
+                    const boarding = await getBoarding(data.boarding);
+                    if (boarding) setBoardingData(boarding);
+                    setVehiclesList(vehicles);
+                } else {
+                    Alert.alert(
+                        "Ocorreu um erro ao carregar dados do embarque",
+                        "Inicie um novo embarque.",
+                        [
+                            {
+                                text: "Ok",
+                                onPress: () => router.back(),
+                            },
+                        ],
+                        { cancelable: false }
+                    );
+                }
+
+                setLoading(false);
+            } catch (err) {
+                console.log("error", err);
+            }
+        };
+
+        loadCheckin();
+    }, [params.checkin_id]);
+
+    if (loading && !checkinData) return <LoadingScreen />;
+
+    return (
+        <>
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <TouchableOpacity onPress={() => router.back()}>
+                        <CloseIcon />
+                    </TouchableOpacity>
+
+                    <Text style={styles.title}>Comprovante de pagamento</Text>
+                    <Text style={styles.date}>
+                        {checkinData?.date_in
+                            ? `${new Date(
+                                  checkinData.date_in
+                              ).toLocaleDateString()} - ${new Date(
+                                  checkinData.date_in
+                              ).toLocaleTimeString()}`
+                            : ""}
+                    </Text>
+                    <Text style={styles.ferryName}>
+                        {boardingData?.ferry_name}
+                    </Text>
+                </View>
+                <View style={styles.value}>
+                    <Text style={styles.valueText}>Valor</Text>
+                    <Text style={styles.valueText}>
+                        {new Intl.NumberFormat("pt-BR", {
+                            style: "currency",
+                            currency: "BRL",
+                        }).format(checkinData?.value || 0)}
+                    </Text>
+                </View>
+                <View style={styles.info}>
+                    <Text style={[styles.infoText, styles.infoTextTitle]}>
+                        Trecho
+                    </Text>
+                    <Text style={styles.infoText}>
+                        {boardingData?.route_name}
+                    </Text>
+                </View>
+                <View style={styles.info}>
+                    <Text style={[styles.infoText, styles.infoTextTitle]}>
+                        Placa
+                    </Text>
+                    <Text style={styles.infoText}>{checkinData?.plate}</Text>
+                </View>
+                <View style={styles.info}>
+                    <Text style={[styles.infoText, styles.infoTextTitle]}>
+                        Tipo
+                    </Text>
+                    <Text style={styles.infoText}>
+                        {checkinData?.vehicle_category_name}:{" "}
+                        {checkinData?.vehicle_name}
+                    </Text>
+                </View>
+                <View style={styles.info}>
+                    <Text style={[styles.infoText, styles.infoTextTitle]}>
+                        Pax
+                    </Text>
+                    <Text style={styles.infoText}>{checkinData?.pax}</Text>
+                </View>
+                <View style={styles.info}>
+                    <Text style={[styles.infoText, styles.infoTextTitle]}>
+                        Obs
+                    </Text>
+                    <Text style={styles.infoText}>
+                        {checkinData?.observation}
+                    </Text>
+                </View>
+
+                {!!checkinData?.add_value && (
+                    <View style={styles.addValues}>
+                        <Text style={styles.addValuesTitle}>
+                            Valores adicionais
+                        </Text>
+
+                        <View
+                            style={[
+                                styles.info,
+                                { borderBottomWidth: 0, paddingHorizontal: 0 },
+                            ]}
+                        >
+                            <Text
+                                style={[styles.infoText, styles.infoTextTitle]}
+                            >
+                                {checkinData?.add_value_reason}
+                            </Text>
+                            <Text style={styles.infoText}>
+                                {new Intl.NumberFormat("pt-BR", {
+                                    style: "currency",
+                                    currency: "BRL",
+                                }).format(checkinData?.add_value || 0)}
+                            </Text>
+                        </View>
+                    </View>
+                )}
+
+                <View style={styles.actions}>
+                    <TouchableOpacity style={styles.action}>
+                        <View style={styles.actionIcon}>
+                            <PrintIcon color="#1A1A1A" />
+                        </View>
+                        <Text style={styles.actionText}>
+                            Imprimir comprovante
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.action}>
+                        <View style={styles.actionIcon}>
+                            <SendIcon color="#1A1A1A" />
+                        </View>
+                        <Text style={styles.actionText}>
+                            Enviar comprovante
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.action}>
+                        <View style={styles.actionIcon}>
+                            <RefundIcon color="#1A1A1A" />
+                        </View>
+                        <Text style={styles.actionText}>
+                            Fazer um reembolso
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+            {loading && <LoadingScreen />}
+        </>
+    );
+};
+
+export default Page;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#FFF",
+    },
+    header: {
+        padding: 16,
+        borderBottomWidth: 1,
+        borderBottomColor: "#ABBED1",
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 600,
+        color: "#212121",
+        marginTop: 32,
+    },
+    date: {
+        color: "#4D4D4D",
+        fontSize: 14,
+        marginTop: 4,
+    },
+    ferryName: {
+        fontSize: 16,
+        fontWeight: 600,
+        color: "#212121",
+        marginTop: 24,
+    },
+    value: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingHorizontal: 16,
+        paddingVertical: 24,
+        paddingBottom: 42,
+        borderBottomWidth: 1,
+        borderBottomColor: "#ABBED1",
+    },
+    valueText: {
+        fontSize: 15,
+        fontWeight: 600,
+        color: "#212121",
+    },
+    info: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: 16,
+        borderBottomWidth: 1,
+        borderBottomColor: "#ABBED1",
+        gap: 32,
+    },
+    infoText: {
+        fontSize: 14,
+        color: "#212121",
+    },
+    infoTextTitle: {
+        fontWeight: 500,
+    },
+    addValues: {
+        padding: 16,
+        paddingBottom: 24,
+        borderBottomWidth: 1,
+        borderBottomColor: "#ABBED1",
+    },
+    addValuesTitle: {
+        fontSize: 15,
+        fontWeight: 700,
+        color: "#4D4D4D",
+    },
+    actions: {
+        paddingVertical: 36,
+        flexDirection: "row",
+        justifyContent: "center",
+        gap: 24,
+    },
+    action: {
+        maxWidth: 86,
+        alignItems: "center",
+        gap: 10,
+    },
+    actionIcon: {
+        width: 48,
+        height: 48,
+        borderRadius: 30,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#E5E5E5",
+    },
+    actionText: {
+        textAlign: "center",
+        fontWeight: 500,
+        fontSize: 12,
+        color: "#717171",
+        width: "100%",
+    },
+});
--- src/components/CheckinDetailsRow/index.tsx
+++ src/components/CheckinDetailsRow/index.tsx
@@ -1,5 +1,12 @@
-import { View, StyleSheet, Text } from "react-native";
+import {
+    View,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+} from "react-native";
 import { CheckinInfos } from "@type/checkins";
+import { router } from "expo-router";
 
 type HeaderContent = {
     col1: string;
@@ -16,83 +23,108 @@ type ComponentProps = {
 };
 
 const Component = (props: ComponentProps) => {
-    return (
-        <View
-            style={[
-                styles.container,
-                !props.isHeader && {
-                    paddingHorizontal: 0,
-                    marginHorizontal: 16,
+    const handleGoToCheckin = () => {
+        if ("id" in props.checkin) {
+            router.push({
+                pathname: "/checkin-details",
+                params: {
+                    checkin_id: props.checkin.id,
                 },
-            ]}
-        >
-            <View style={[styles.column, { width: "20%" }]}>
-                <Text
-                    style={[
-                        styles.content,
-                        props.isHeader && styles.contentHeader,
-                        props.isFooter && styles.contentFooter,
-                    ]}
-                >
-                    {"plate" in props.checkin
-                        ? props.checkin.plate
-                        : props.checkin.col1}
-                </Text>
-            </View>
-            <View style={[styles.column, { width: "50%" }]}>
-                <Text
-                    style={[
-                        styles.content,
-                        props.isHeader && styles.contentHeader,
-                        props.isFooter && styles.contentFooter,
-                    ]}
-                >
-                    {"vehicle_name" in props.checkin
-                        ? props.checkin.vehicle_name
-                        : props.checkin.col2}
-                </Text>
-            </View>
-            <View style={[styles.column, { width: "10%" }]}>
-                <Text
-                    style={[
-                        styles.content,
-                        { textAlign: "center" },
-                        props.isHeader && styles.contentHeader,
-                        props.isFooter && styles.contentFooter,
-                    ]}
-                >
-                    {"pax" in props.checkin
-                        ? props.checkin.pax
-                        : props.checkin.col3}
-                </Text>
-            </View>
-            <View style={[styles.column, { width: "20%" }]}>
-                <Text
-                    style={[
-                        styles.content,
-                        { textAlign: "right" },
-                        props.isHeader && styles.contentHeader,
-                        props.isFooter && styles.contentFooter,
-                    ]}
-                >
-                    {"value" in props.checkin
-                        ? new Intl.NumberFormat("pt-BR", {
-                              style: "currency",
-                              currency: "BRL",
-                          }).format(props.checkin.value)
-                        : props.checkin.col4}
-                </Text>
-            </View>
+            });
+        }
+    };
 
-            {!!props.showReason &&
-                !props.isHeader &&
-                !props.isFooter &&
-                "add_value_reason" in props.checkin && (
-                    <Text style={styles.addValueDescription}>
-                        {props.checkin.add_value_reason}
+    return (
+        <TouchableWithoutFeedback
+            onPress={() => "plate" in props.checkin && handleGoToCheckin()}
+        >
+            <View
+                style={[
+                    styles.container,
+                    !props.isHeader && {
+                        paddingHorizontal: 0,
+                        marginHorizontal: 16,
+                    },
+                ]}
+            >
+                <View style={[styles.column, { width: "20%" }]}>
+                    {"plate" in props.checkin ? (
+                        <Text
+                            style={[
+                                styles.content,
+                                props.isHeader && styles.contentHeader,
+                                props.isFooter && styles.contentFooter,
+                            ]}
+                        >
+                            {props.checkin.plate}
+                        </Text>
+                    ) : (
+                        <Text
+                            style={[
+                                styles.content,
+                                props.isHeader && styles.contentHeader,
+                                props.isFooter && styles.contentFooter,
+                            ]}
+                        >
+                            {props.checkin.col1}
+                        </Text>
+                    )}
+                </View>
+                <View style={[styles.column, { width: "50%" }]}>
+                    <Text
+                        style={[
+                            styles.content,
+                            props.isHeader && styles.contentHeader,
+                            props.isFooter && styles.contentFooter,
+                        ]}
+                    >
+                        {"vehicle_name" in props.checkin
+                            ? props.checkin.vehicle_name
+                            : props.checkin.col2}
                     </Text>
-                )}
-        </View>
+                </View>
+                <View style={[styles.column, { width: "10%" }]}>
+                    <Text
+                        style={[
+                            styles.content,
+                            { textAlign: "center" },
+                            props.isHeader && styles.contentHeader,
+                            props.isFooter && styles.contentFooter,
+                        ]}
+                    >
+                        {"pax" in props.checkin
+                            ? props.checkin.pax
+                            : props.checkin.col3}
+                    </Text>
+                </View>
+                <View style={[styles.column, { width: "20%" }]}>
+                    <Text
+                        style={[
+                            styles.content,
+                            { textAlign: "right" },
+                            props.isHeader && styles.contentHeader,
+                            props.isFooter && styles.contentFooter,
+                        ]}
+                    >
+                        {"value" in props.checkin
+                            ? new Intl.NumberFormat("pt-BR", {
+                                  style: "currency",
+                                  currency: "BRL",
+                              }).format(props.checkin.value)
+                            : props.checkin.col4}
+                    </Text>
+                </View>
+
+                {!!props.showReason &&
+                    !props.isHeader &&
+                    !props.isFooter &&
+                    "add_value_reason" in props.checkin && (
+                        <Text style={styles.addValueDescription}>
+                            {props.checkin.add_value_reason}
+                        </Text>
+                    )}
+            </View>
+        </TouchableWithoutFeedback>
     );
 };
 
